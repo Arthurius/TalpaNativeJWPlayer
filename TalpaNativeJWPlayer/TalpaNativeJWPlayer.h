f9ed3c4014c4e8f540aa12ca22948ac714d59142
Arthur Schenk <arthur@openweb.nl>
2019-04-08 16:26:23 +0200

    Initial commit

---
+++ TalpaNativeJWPlayer/TalpaNativeJWPlayer.h
@@ -0,0 +1,19 @@
+//
+//  TalpaNativeJWPlayer.h
+//  TalpaNativeJWPlayer
+//
+//  Created by Arthur Schenk on 08/04/2019.
+//  Copyright © 2019 Talpa Network. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TalpaNativeJWPlayer.
+FOUNDATION_EXPORT double TalpaNativeJWPlayerVersionNumber;
+
+//! Project version string for TalpaNativeJWPlayer.
+FOUNDATION_EXPORT const unsigned char TalpaNativeJWPlayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TalpaNativeJWPlayer/PublicHeader.h>
+
+
